c0a44f62047d824bac2ffbc53dc04bb3bab3670a
student <abdullahihamdiabass@gmail.com>
2021-03-02 14:17:33 +0300

    first commit

---
+++ src/halima.kt
@@ -0,0 +1,32 @@
+fun main() {
+    println("Hello hamdi")
+    modulus()
+    addition()
+    println("hamdi is good")
+
+}
+fun halima() {
+    println("Hello halima")
+
+}
+fun modulus() {
+    var num1=20
+    var num4=10
+    var sum=num1/num4
+    println(sum)
+
+
+}
+fun addition() {
+    var num3=50
+    var num5=60
+    var num7=70
+    var num8=10
+    var sum= num3+num5+num7+num8
+    println(sum)
+
+}
+fun me() {
+    println("hamdi is good")
+
+}
